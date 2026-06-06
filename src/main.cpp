c600518c3b62351f1a8b6ed34451e83c12f1c0b0
Marco HB <marcoxhbx18@gmail.com>
2026-06-06 13:08:58 -0500

    Ajusté el include de treap.h y formateé la función main

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,42 @@
+#include "../include/treap.h"
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
-#include "treap.h"
 
 // por ahora solo el esqueleto, mañana le meto la implementacion del treap
 
 int main() {
-    srand(time(0));
+  srand(time(0));
 
-    int q;
-    scanf("%d", &q);
+  int q;
+  scanf("%d", &q);
 
-    Nodo* raiz = nullptr;
+  Nodo *raiz = nullptr;
 
-    while (q--) {
-        char op;
-        int x;
-        scanf(" %c %d", &op, &x);
+  while (q--) {
+    char op;
+    int x;
+    scanf(" %c %d", &op, &x);
 
-        if (op == 'I') {
-            // insertar x en el conjunto
-            insertar(raiz, x);
-        }
-        else if (op == 'D') {
-            // eliminar x del conjunto
-            eliminar(raiz, x);
-        }
-        else if (op == 'K') {
-            // buscar el k-esimo menor
-            int total = obtenerTam(raiz);
-            if (x < 1 || x > total) {
-                printf("invalid\n");
-            } else {
-                printf("%d\n", kesimo(raiz, x));
-            }
-        }
-        else if (op == 'C') {
-            // contar cuantos son menores que x
-            printf("%d\n", contarMenores(raiz, x));
-        }
+    if (op == 'I') {
+      // insertar x en el conjunto
+      insertar(raiz, x);
+    } else if (op == 'D') {
+      // eliminar x del conjunto
+      eliminar(raiz, x);
+    } else if (op == 'K') {
+      // buscar el k-esimo menor
+      int total = obtenerTam(raiz);
+      if (x < 1 || x > total) {
+        printf("invalid\n");
+      } else {
+        printf("%d\n", kesimo(raiz, x));
+      }
+    } else if (op == 'C') {
+      // contar cuantos son menores que x
+      printf("%d\n", contarMenores(raiz, x));
     }
+  }
 
-    return 0;
+  return 0;
 }
